4f541860ee7e46623602cabb87d39d707cd2489f
UserLazy <78595134+UserLazy@users.noreply.github.com>
2021-11-24 08:48:43 +0700

    Update markup_button.py

--- functions/markup_button.py
+++ functions/markup_button.py
@@ -44,7 +44,7 @@ def start_markup(chat_id: int, bot_username: str):
                     gm(chat_id, "helpbutton"), callback_data="cbhelp"
                 ),
                 InlineKeyboardButton(
-                    gm(chat_id, "maintainer"), url="https://t.me/talktoabdul_bot"
+                    gm(chat_id, "maintainer"), url="https://t.me/RxyMX"
                 ),
             ],
             [
@@ -58,7 +58,7 @@ def start_markup(chat_id: int, bot_username: str):
             [
                 InlineKeyboardButton(
                     gm(chat_id, "source_code"),
-                    url="https://github.com/DoellBarr/solidmusic",
+                    url="https://github.com/",
                 )
             ],
         ]
